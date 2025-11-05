b03a7367ff24616912874abe582cfc91e4e0ad9b
Patrickkk2005 <neagupatrick2005@gmail.com>
2025-11-05 21:33:48 +0200

    create table fara parser

--- EngineHeaders.h
+++ EngineHeaders.h
@@ -52,12 +52,12 @@ class Column {
 
   public:
 	Column() {
-		ColumnName = nullptr;
-		type = UNKNOW;
-		size = 0;
-		defaultValue = nullptr;
+		this->ColumnName = nullptr;
+		this->type = UNKNOW;
+		this->size = 0;
+		this->defaultValue = nullptr;
 
-		cout << "Default constructor used";
+		cout << "Default constructor used" << endl;
 	}
 
 	Column(const char *ColumnName, const char *typeString, int size, const char *defaultValue) {
@@ -70,6 +70,30 @@ class Column {
 		setDefVal(defaultValue);
 
 		cout << "Para Constructor used" << endl;
+	}
+
+	Column(const Column &other) {
+		this->ColumnName = nullptr;
+		this->defaultValue = nullptr;
+
+		deepCopyFunc(this->ColumnName, other.ColumnName);
+		deepCopyFunc(this->defaultValue, other.defaultValue);
+
+		this->size = other.size;
+		this->type = other.type;
+	}
+
+	Column &operator=(const Column &other) {
+		if (this == &other) {
+			return *this;
+		}
+		deepCopyFunc(this->ColumnName, other.ColumnName);
+		deepCopyFunc(this->defaultValue, other.defaultValue);
+
+		this->size = other.size;
+		this->type = other.type;
+
+		return *this;
 	}
 
 	~Column() {
@@ -119,4 +143,190 @@ class Column {
 		this->type = newtype;
 	}
 
+	friend ostream &operator<<(ostream &os, const Column &col) {
+		os << "Column Name: " << col.ColumnName << endl
+		   << ", Type: " << col.type << endl
+		   << ", Size: " << col.size << endl
+		   << ", Default Value: " << col.defaultValue << endl;
+		return os;
+	}
+
+	bool operator==(const Column &other) {
+		return (this->ColumnName != nullptr && other.ColumnName != nullptr && strcmp(this->ColumnName, other.ColumnName) == 0) && this->type == other.type && this->size == other.size;
+	}
+
+	explicit operator const char *() const {
+		return this->ColumnName;
+	}
+
+	bool operator!() const {
+		return this->ColumnName == nullptr;
+	}
+
+	Column operator+(int value) const {
+		Column newCol = *this;
+		newCol.size = this->size + value;
+		return newCol;
+	}
+
 }; // end of Column class
+
+class CreateTableCMD {
+  private:
+	string tableName;
+	bool IFnotExist;
+	Column *columns;
+	int columnCnt;
+	int columnCap;
+
+  public:
+	static int commandCounter;
+	const int cmdID;
+
+	CreateTableCMD() : cmdID(++commandCounter) {
+		this->IFnotExist = false;
+		this->columns = nullptr;
+		this->columnCnt = 0;
+		this->columnCap = 0;
+	}
+
+	CreateTableCMD(const char *name, bool ifq = false) : cmdID(++commandCounter) {
+		this->tableName = name;
+		if (tableName.empty()) {
+			throw "Name cannot be empty!";
+		}
+		this->IFnotExist = ifq;
+		this->columns = nullptr;
+		this->columnCnt = 0;
+		this->columnCap = 0;
+	}
+
+	CreateTableCMD(const CreateTableCMD &otherTable) : cmdID(++commandCounter) {
+		this->tableName = otherTable.tableName;
+		this->IFnotExist = otherTable.IFnotExist;
+		this->columnCnt = otherTable.columnCnt;
+		this->columnCap = otherTable.columnCap;
+
+		this->columns = new Column[this->columnCap];
+		for (int i = 0; i < columnCnt; i++) {
+			this->columns[i] = otherTable.columns[i];
+		}
+	}
+
+	CreateTableCMD &operator=(const CreateTableCMD &otherTable) {
+		if (this == &otherTable) {
+			return *this;
+		}
+
+		delete[] this->columns;
+
+		this->tableName = otherTable.tableName;
+		this->IFnotExist = otherTable.IFnotExist;
+		this->columnCnt = otherTable.columnCnt;
+		this->columnCap = otherTable.columnCap;
+
+		this->columns = new Column[this->columnCap];
+		for (int i = 0; i < columnCnt; i++) {
+			this->columns[i] = otherTable.columns[i];
+		}
+
+		return *this;
+	}
+
+	~CreateTableCMD() {
+		delete[] this->columns;
+	}
+
+	void addColumn(const Column &col) {
+		if (columnCnt >= columnCap) {
+			int newCap;
+			if (columnCap == 0) {
+				newCap = 5;
+			} else
+				newCap = newCap * 2;
+			Column *newCol = new Column[newCap];
+
+			if (columns != nullptr)
+				for (int i = 0; i < newCap; i++) {
+					newCol[i] = this->columns[i];
+				}
+
+			delete[] this->columns;
+
+			this->columns = newCol;
+			this->columnCap = newCap;
+		}
+		this->columns[columnCnt++] = col;
+	}
+
+	// GETTERS:
+
+	string getTableName() {
+		return this->tableName;
+	}
+
+	int getColCnt() {
+		return this->columnCnt;
+	}
+
+	Column &operator[](int i) {
+		if (i < 0 || i >= columnCnt) {
+			throw "Invalid Index";
+		}
+		return columns[i];
+	}
+
+	friend ostream &operator<<(ostream &os, const CreateTableCMD &cmd) {
+		os << endl
+		   << "Create Table ID: " << cmd.cmdID << endl
+		   << "Table Name: " << cmd.tableName << endl
+		   << "Column Count: " << cmd.columnCnt << endl
+		   << "Columns: " << endl;
+		for (int i = 0; i < cmd.columnCnt; i++) {
+			os << "	" << i + 1 << ". " << cmd.columns[i] << endl;
+		}
+		os << "_______________________________________" << endl;
+		return os;
+	}
+
+}; // end of create table command class
+
+class CreateTableParser {
+  public:
+	CreateTableCMD *parsing(string query) {
+	}
+
+}; // end of parser class
+
+class MainEngine {
+  public:
+	void runapp() {
+		string input;
+		while (true) {
+			cout << "sql>> ";
+			getline(cin, input);
+			if (input == "exit") {
+				cout << "goodbye!!!";
+				break;
+			}
+			if (input.empty()) {
+				continue;
+			}
+		}
+	}
+
+  private:
+	void processCMD(string &command) {
+		string upperC;
+		for (char &c : upperC) {
+			c = toupper(c);
+		}
+		if (upperC.rfind("CREATE TABLE", 0) == 0) {
+			CreateTableParser parser;
+			CreateTableCMD *cmd = nullptr;
+			// cmd=parser.parse(command);
+			cout << *cmd;
+		} else
+			cout << "Command not recognized!";
+	}
+};
--- main.cpp
+++ main.cpp
@@ -5,9 +5,41 @@
 #include <string>
 using namespace std;
 
+int CreateTableCMD::commandCounter = 0;
+
 int main() {
-	Column col1("boja", "STRING", 10, "bussy");
-	cout << col1.getName() << endl
-		 << col1.getSize() << endl
-		 << col1.getDefVal();
+	Column col1("userID", "NUMBER", 10, "0"); // test with param constructor
+	cout << "col1: " << col1 << endl;
+	Column col2; // normal assig
+	col2.setName("username");
+	col2.setDatatype("DATE");
+	col2.setSize(50);
+	col2.setDefVal("guest");
+	cout << "col2: " << col2 << endl;
+	Column col3 = col1;
+	cout << "col3 (copy of col1): " << col3 << endl;
+	col2 = col1;
+	cout << "col2 (assigned from col1): " << col2 << endl;
+	cout << "Is col2 ! (invalid)? " << (!col2) << endl;
+	cout << "col3 + 5: " << (col3 + 5) << endl;
+	cout << "=================================================";
+	// START OF SECOND TEST
+	Column c1("ID", "NUMBER", 4, "0");
+	Column c2("NAME", "STRING", 50, "Anonymous");
+	Column c3("BIRTH_DATE", "DATE", 8, "CURRENT_DATE");
+	CreateTableCMD command1("Users", true);
+	command1.addColumn(c1);
+	command1.addColumn(c2);
+	command1.addColumn(c3);
+
+	cout << command1;
+
+	CreateTableCMD command2 = command1;
+
+	cout << command2;
+
+	// REAL TEST
+
+	MainEngine engine;
+	engine.runapp();
 }
